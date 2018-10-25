728997d6cd9e46d7d2fabd2d57611b91aa706df6
montaz708 <alex.vazquez708@gmail.com>
2018-10-25 17:49:08 +0000

    More work

--- CSC241/Assignment2/Cashiers.java
+++ CSC241/Assignment2/Cashiers.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 public class Cashiers{
-  static int matrix[][] = new int[3][6];
+  static String matrix[][] = new String[3][6];
   static int currentSize[] = new int[3];
-  static int total;
+  static int total = 1;
 
   //The below method will add the next value to a queue so long as it has space.
   public static boolean enqueue(){
@@ -10,62 +12,123 @@ public class Cashiers{
     int pos = 0;
     boolean bool = false;
 
-    /*The idea here is to iterate through currentSize to see if all values
-    are equal to the max size of the arrays. Additionally, we will keep
-    track of the smallest queue, and if bool is still false, we'll push
-    another "customer" onto the queue.*/
-
     for(int a = 0; a < currentSize.length; a++){
       if(currentSize[a] == 6){
         count++;
       }
-      else if(currentSize[a] < max){
+      if(count == 3){
+        bool = true;
+        System.out.println("All the queues are full.");
+        return bool;
+        break;
+      }
+      if(currentSize[a] == max){
+        
+      }
+      if(currentSize[a] < max){
         max = currentSize[a];
         pos = a;
       }
-      if(currentSize[a] == currentSize[a-1]){
-        pos = a-1;
-      }
-      if(a = 2 && currentSize[a] == currentSize[a-2]){
-        pos = a -2;
-      }
+
     }
-    if(count == 3){
-      bool = true;
-      return bool;
-    }
+
+
 
 
 
     return bool;
   }
   public static int dequeue(int which){
-
+    int pos = 0;
+    int count = 0;
+    int popped = 0;
+    for(int a = 0; a < matrix[which].length; a++){
+      if((matrix[which][a]).contains("F")){
+        popped = Integer.parseInt(matrix[which][a].replace("F",""));
+        pos = a;
+        while(matrix[which][count] == "0"){
+          count++;
+        }
+        matrix[which][count] = matrix[which][count]+"F";
+        currentSize[which] = currentSize[which] - 1;
+      }
+    }
+    return popped;
   }
 
   //Below will reset all the values of the matrix array to 0.
   public static void clear(){
-    for(int a = 0; a < matrix.length(); a++){
-      for(int b 0; matrix[a].length; b++){
-        matrix[a][b] = 0;
+    for(int a = 0; a < matrix.length; a++){
+      for(int b = 0; b < matrix[a].length; b++){
+        matrix[a][b] = "0";
       }
     }
   }
 
   //The below will display the contents of the matrix variable.
   public static void display(){
-    for(int a = 0; a < matrix.length(); a++){
-      for(int b = 0; b < matrix[a].length(); b++){
+    if(matrix[0][0] == null){
+      clear();
+    }
+    for(int a = 0; a < matrix.length; a++){
+      for(int b = 0; b < matrix[a].length; b++){
         System.out.print(matrix[a][b]+ "    ");
       }
       System.out.println();
     }
   }
+  //The function below will fill the matrix, from left to right incrementing
+  //each element.
   public static void fill(){
-
+    for(int a = 0; a < matrix.length; a++){
+      for(int b = 0; b < matrix[a].length; b++){
+        matrix[a][b] = Integer.toString(total);
+        if(b == 0){
+          matrix[a][b] = matrix[a][b] + "F";
+        }
+        if(b == 5){
+          matrix[a][b] = matrix[a][b] + "B";
+        }
+        total ++;
+        currentSize[a] = currentSize[a] + 1;
+      }
+    }
   }
 
   public static void main(String [] args){
+    Scanner in = new Scanner(System.in);
+    String input = null;
+
+    while(input != "Q"){
+      display();
+      System.out.println("Input command E or 1 or 2 or 3 or F or C or Q");
+      input = (in.nextLine()).toUpperCase();
+      switch (input){
+        case "E":
+          enqueue();
+          break;
+        case "1":
+          dequeue(0);
+          break;
+        case "2":
+          dequeue(1);
+          break;
+        case("3"):
+          dequeue(2);
+          break;
+        case "F":
+          fill();
+          break;
+        case "C":
+          clear();
+          break;
+        case "Q":
+          input = "Q";
+          break;
+        default:
+          System.out.println("That's not a valid command, try again:");
+      }
+    }
 
   }
 }
