b3067814e5f313d8dde249eae3a2a6571f4dd761
montaz708 <alex.vazquez708@gmail.com>
2018-11-24 20:32:06 -0500

    Was able to figure out the shellsort with an example, and am able to ouput the result to the console

--- CSC241/Assignment3/InternetUsers.java
+++ CSC241/Assignment3/InternetUsers.java
@@ -2,6 +2,7 @@ package assignment3;
 import java.io.PrintWriter;
 import java.io.File;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.Random;
 
 public class InternetUsers{
@@ -12,9 +13,12 @@ public class InternetUsers{
 		String path = home + File.separator + "Downloads" + File.separator;
 		Scanner stdIn = new Scanner(new File(path + "CountrySortedAlpha.txt"));
 		PrintWriter out = new PrintWriter(path + "CountryUnsortedAlpha.txt");
-		String [] lineSort = new String [201];
+		String [][] lineSort = new String [2][201];
+		String [] popString = new String [201];
 		String [] line = new String[201];
 		String temp;
+		String build;
+		boolean bool;
 		Random rand = new Random();
 		
 		/*--------------Begin Randomize part-------------------*/
@@ -32,38 +36,50 @@ public class InternetUsers{
 			}
 			temp = line[where];
 			out.println(temp);
+			StringTokenizer str = new StringTokenizer(temp);
+			build = "";
+			bool = false;
+			do {
+				try {
+					temp = str.nextToken();
+					Integer.parseInt(temp);
+					lineSort[1][k] = temp;
+					bool = true;
+				}
+				catch(NumberFormatException e) {
+					build = build + " " + temp;
+				}
+			}while(bool == false);
+			lineSort[0][k] = build;
 			line[where] = "";
 			
 		}
 		/*---------------End Randomize part-------------------*/
-		
-		
-		
-		
 		stdIn.close();
 		out.close();
+		shellSort(lineSort);
+		
+		for(int a = 0; a < lineSort[0].length; a++) {
+			System.out.println(lineSort[0][a] + "\t" + lineSort[1][a]);
+		}
+		
 	}
 	
-	public static int[] shellSort(int[] arr) {
-		int increment = arr.length / 2;
-		while(increment> 0) {
-			for(int i = increment; i < arr.length; i++) {
-				int j = i;
-				int temp = arr[i];
-				while(j >= increment && arr[j - increment] > temp) {
-					arr[j] = arr[j - increment];
-					j = j - increment;
+	
+	public static void shellSort(String[][] a){
+		String elem;
+		int j;
+		for(int gap = a[1].length/2; gap > 0; gap /=2) {
+			for(int i = gap; i < a[1].length; i++) {
+				elem = a[0][i];
+				int temp = Integer.parseInt(a[1][i]);
+				for( j = i; j >= gap && temp > Integer.parseInt(a[1][j - gap]); j-=gap) {
+					a[0][j] = a[0][j - gap];
+					a[1][j] = a[1][j - gap];
 				}
-				arr[j] = temp;
-			}
-			if (increment == 2) {
-				increment = 1;
-			}
-			else {
-				increment *= (5.0/11);
+				a[0][j] = elem;
+				a[1][j] = Integer.toString(temp);
 			}
 		}
-		return arr;
 	}
-
 }
