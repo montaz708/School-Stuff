875005fa922758937ac8bc3bb11e7b180981166c
Alex Vazquez <alex.vazquez708@gmail.com>
2019-02-04 16:52:20 -0500

    First iteration of this program

---
+++ CSC331/Assignment 1/my-cat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define MAXCHAR 1000
+int main(int argc, char* argv[]){
+    char str[MAXCHAR];
+    if(argc == 1)
+        return 0;
+    if(argc > 1){
+        int counter = 1;
+        FILE *fptr;
+        do{
+            if(fptr = fopen(argv[counter], 'r') == NULL){
+                printf("my-cat: cannot open file");
+                exit(1);
+            }
+            while (fgets(str, MAXCHAR, fptr) != NULL){
+                prtinf("%s", str);
+            }
+            fclose(fptr);
+            counter++;
+        }while(counter < argc);
+    }
+    return 0;
+}
