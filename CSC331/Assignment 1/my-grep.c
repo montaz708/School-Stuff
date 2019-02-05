9ec9589759b70cb6458fc950dc4e2f515c4ac0f1
Alex Vazquez <alex.vazquez708@gmail.com>
2019-02-04 23:30:09 -0500

    First commit for this program file

---
+++ CSC331/Assignment 1/my-grep.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXCHAR 1000
+int main(int argc, char* argv[]){
+    char str[MAXCHAR];
+    if(argc == 1)
+        printf("my-grep: searchterm [file ...]");
+        exit(1);
+    if(argc >= 2){
+        char search [] = argv[1];
+        int counter = 2;
+        FILE *fptr;
+        do{
+            if(fptr = fopen(argv[counter], "r") == NULL){
+                printf("my-grep: cannot open file \n");
+                exit(1);
+            }
+            while(fgets(str, MAXCHAR, fptr) != NULL){
+                if(str == search){
+                    printf(str);
+                }
+            }
+        }while(counter < argc);
+    }
+}
