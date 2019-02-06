6c1d9eef174856d0d10c835566950f107c1f13ee
Alex Vazquez <alex.vazquez708@gmail.com>
2019-02-05 20:25:57 -0500

    testing

--- CSC331/Assignment 1/my-grep.c
+++ CSC331/Assignment 1/my-grep.c
@@ -1,30 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
 
-#define MAXCHAR 1000
+// #define MAXCHAR 1000
+// int main(int argc, char* argv[]){
+//     char str[MAXCHAR];
+//     if(argc == 1)
+//         printf("my-grep: searchterm [file ...]");
+//         exit(1);
+//     if(argc > 2){
+//         int counter = 2;
+//         FILE *fptr;
+//         char* search;
+//         do{
+//             if((fptr = fopen(argv[counter], "r")) == NULL){
+//                 printf("my-grep: cannot open file \n");
+//                 exit(1);
+//             }
+//             while(fgets(str, MAXCHAR, fptr) != NULL){
+//                 if(str == search){
+//                     printf("%s",str);
+//                 }
+//             }
+//             counter++;
+//         }while(counter < argc);
+//     }
+// }
+
+
+
 int main(int argc, char* argv[]){
-    char str[MAXCHAR];
-    if(argc == 1)
+    char *line = NULL;
+    char *word = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    FILE *fptr;
+
+    if(argc == 1){
         printf("my-grep: searchterm [file ...]");
         exit(1);
-    if(argc >= 2){
+    }
+    else if(argc == 2){
+        //do stdin
+    }
+    else if(argc > 2){
         int counter = 2;
-        FILE *fptr;
-        char* search;
-        strcpy(search, (char*)argv[1]);
-        printf("%s",search);
+        word = (char *)malloc(sizeof(argv[1]));
+        strcpy(*word, argv[1]);
+        
         do{
-            if((fptr = fopen(argv[counter], "r")) == NULL){
-                printf("my-grep: cannot open file \n");
-                exit(1);
-            }
-            while(fgets(str, MAXCHAR, fptr) != NULL){
-                if(str == search){
-                    printf("%s",str);
+            fptr = fopen(argv[counter], "r");
+            while((nread = getline(&line, &len, fptr)) != -1){
+                if(strstr(line, word) != NULL){
+                    printf("%s", line);
                 }
             }
+            fclose(fptr);
             counter++;
         }while(counter < argc);
+        free(line);
+        return 0;
     }
 }
