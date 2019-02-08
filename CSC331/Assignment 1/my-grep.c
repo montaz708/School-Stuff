44efa0d5ee409578ccab9907aa3f04b5720cfe64
Alex Vazquez <alex.vazquez708@gmail.com>
2019-02-07 20:03:05 -0500

    Good stuff

--- CSC331/Assignment 1/my-grep.c
+++ CSC331/Assignment 1/my-grep.c
@@ -2,32 +2,6 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <string.h>
-// #define MAXCHAR 1000
-// int main(int argc, char* argv[]){
-//     char str[MAXCHAR];
-//     if(argc == 1)
-//         printf("my-grep: searchterm [file ...]");
-//         exit(1);
-//     if(argc > 2){
-//         int counter = 2;
-//         FILE *fptr;
-//         char* search;
-//         do{
-//             if((fptr = fopen(argv[counter], "r")) == NULL){
-//                 printf("my-grep: cannot open file \n");
-//                 exit(1);
-//             }
-//             while(fgets(str, MAXCHAR, fptr) != NULL){
-//                 if(str == search){
-//                     printf("%s",str);
-//                 }
-//             }
-//             counter++;
-//         }while(counter < argc);
-//     }
-// }
-
-
 
 int main(int argc, char* argv[]){
     char *line = NULL;
@@ -37,15 +11,25 @@ int main(int argc, char* argv[]){
     FILE *fptr;
 
     if(argc == 1){
-        printf("my-grep: searchterm [file ...]");
+        printf("my-grep: searchterm [file ...] \n");
         exit(1);
     }
     else if(argc == 2){
-        //do stdin
+        word = (char *)calloc(strlen(argv[1]), sizeof(char*));
+        strcpy(word, argv[1]);
+        printf("Type a line to search: ");
+        line = getline(&line, &len, 0);
+        if(strstr(line, word) != NULL){
+            printf("%s", line);
+            return 0;
+        }
     }
     else if(argc > 2){
         int counter = 2;
-        word = (char *)malloc(sizeof(argv[1]));
+        if(sizeof(argv[1]) == ""){
+            return 0;
+        }
+        word = (char *)calloc(strlen(argv[1]), sizeof(char*));
         strcpy(word, argv[1]);
         
         do{
@@ -59,6 +43,7 @@ int main(int argc, char* argv[]){
             counter++;
         }while(counter < argc);
         free(line);
+        free(word);
         return 0;
     }
 }
