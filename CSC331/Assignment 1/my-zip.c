cc08ae5958c0290a8e7e2be1377d7f72a7a0af62
Alex Vazquez <alex.vazquez708@gmail.com>
2019-02-18 12:02:44 -0500

    Good stuff

--- CSC331/Assignment 1/my-zip.c
+++ CSC331/Assignment 1/my-zip.c
@@ -9,22 +9,37 @@ int main(int argc, char *argv[]){
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
-
-	fp = fopen(argv[1], "r");
-	while( (nread = getline(&line, &len, fp)) != -1){
-		i = 0;
-		while(i < strlen(line)){
-			if(i > 0){
-				if(line[i-1] == line[i]){
-					word_count++;
-				}
-				else{
-					printf("%d%c", ++word_count, line[i - 1]);
-					word_count = 0;
+	int a;
+	if(argc < 2){
+		printf("my-zip: file1 [file2 ...] \n");
+		return(1);
+	}
+	for(a = 1; a < argc; a++){
+		fp = fopen(argv[a], "r");
+		if(fp != NULL){
+			while( (nread = getline(&line, &len, fp)) != -1){
+				i = 0;
+				while(i < strlen(line)){
+					if(i > 0){
+						if(line[i-1] == line[i]){
+							word_count++;
+						}
+						else{
+							printf("%d%c", ++word_count, line[i - 1]);
+							word_count = 0;
+						}
+					}
+					i++;
+					printf("\n");
 				}
 			}
-		i++;
+			fclose(fp);
+			free(line);
+		}
+		else{
+			printf("Could not open %s for compressing!", argv[a]);
+			return(1);
 		}
 	}
-
+	return(0);
 }
