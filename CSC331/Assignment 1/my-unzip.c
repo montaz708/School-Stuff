7f73304a414849699088f0c39d84373e6a1cf91f
montaz708 <alex.vazquez708@gmail.com>
2019-02-19 20:38:17 -0500

    Commenting

--- CSC331/Assignment 1/my-unzip.c
+++ CSC331/Assignment 1/my-unzip.c
@@ -12,23 +12,23 @@ int main(int argc, char *argv[]){
         printf("my-unzip: file1 [file2 ...] \n");
         return(1);
     }
-    for(int a = 1; a < argc; a++){
-        fp = fopen(argv[a], "r");
+    for(int a = 1; a < argc; a++){ //Iterate for all files passed from stdin
+        fp = fopen(argv[a], "r"); //Open file for reading
         if(fp != NULL){
             do{
-                fread(&count, 4, 1, fp);
+                fread(&count, 4, 1, fp); //Read from open file the first integer, and the character
                 fread(&character, 1, 1, fp);
-                if( feof(fp)){
+                if( feof(fp)){ // If EOF, break from infinite loop
                     break;
                 }
-                for(int b = 0; b <= count; b++){
+                for(int b = 0; b <= count; b++){ //For duration of count, print inline the character that was read.
                     fputc(character, stdout);
                 }
 
             }while(1);
         }
         else{
-            printf("Could not read %s for file decompression!", argv[a]);
+            printf("Could not read %s for file decompression!", argv[a]); //Handles the case that a file cannot be opened.
         }
         fclose(fp);
     }
--- CSC331/Assignment 1/my-zip.c
+++ CSC331/Assignment 1/my-zip.c
@@ -11,23 +11,26 @@ int main(int argc, char *argv[]){
 		printf("my-zip: file1 [file2 ...] \n");
 		return(1);
 	}
+	/*Since one of the requirements was storing the character in it's ASCII value
+	I read online that storing the character in a uint8_t variable would store it that way,
+	which is why I use getc and that data type to store the character.*/
 	for(int a = 1; a < argc; a++){
 		fp = fopen(argv[a], "r");
 		if(fp != NULL){
-			character = fgetc(fp);
+			character = fgetc(fp); //Read in the first character to do a comparison on the next
 			count = 0;
 			do{
-				new_character = fgetc(fp);
-				if( feof(fp) ){
+				new_character = fgetc(fp); //Read in the next character from specified file
+				if( feof(fp) ){ // Conditional statement for breaking out of infinite loop (if end of file{break})
 					break;
 				}
-				if(character == new_character){
+				if(character == new_character){ //If they're equal, increment a counter to keep track of how many there are
 					count++;
 				}
-				else{
-					fwrite(&count, 4, 1, stdout);
+				else{ // When the comparison is no longer equal between two characters, write to stdout
+					fwrite(&count, 4, 1, stdout); // Write the value at count, allocating four bytes, written only once, to stdout
 					fwrite(&character, 1, 1, stdout);
-					character = new_character;
+					character = new_character; // Read in the next character, reset count and continue looping
 					count = 0;
 				}
 			}while(1); // Loop for interating over all characters of a file
