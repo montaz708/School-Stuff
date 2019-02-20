635df4c6a8eb6ae8d3691f1262d75859a3842ad0
montaz708 <alex.vazquez708@gmail.com>
2019-02-19 20:30:01 -0500

    Commenting

--- CSC331/Assignment 1/my-cat.c
+++ CSC331/Assignment 1/my-cat.c
@@ -3,22 +3,23 @@
 #define MAXCHAR 1000
 int main(int argc, char* argv[]){
     char str[MAXCHAR];
-    if(argc == 1)
+    if(argc == 1){ //Handles the case where no arguments were passed
         return 0;
+    }
     if(argc > 1){
         int counter = 1;
         FILE *fptr;
         do{
-            if((fptr = fopen(argv[counter], "r")) == NULL){
+            if((fptr = fopen(argv[counter], "r")) == NULL){ //Handles the case where an invalid file name was passed.
                 printf("my-cat: cannot open file \n");
                 exit(1);
             }
-            while (fgets(str, MAXCHAR, fptr) != NULL){
+            while (fgets(str, MAXCHAR, fptr) != NULL){  //Loop through the strings of the file until a null pointer is returned. Print to stdout the string
                 printf("%s", str);
             }
             fclose(fptr);
             counter++;
-        }while(counter < argc);
+        }while(counter < argc); //Loop established to print out multiple file contents
     }
     return 0;
 }
