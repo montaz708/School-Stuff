e167224835af4d874b56f679b4e08823b9601a7f
montaz708 <alex.vazquez708@gmail.com>
2019-02-19 20:19:24 -0500

    Finally got it

--- CSC331/Assignment 1/my-unzip.c
+++ CSC331/Assignment 1/my-unzip.c
@@ -1,47 +1,31 @@
 #include <string.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[]){
     FILE *fp;
-    int i;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t  nread;
-    char nums[20];
-    int word_count, loops;
-    char *tok;
+    uint8_t character;
+    uint8_t count;
 
     if(argc < 2){
         printf("my-unzip: file1 [file2 ...] \n");
         return(1);
     }
-    int a;
-    for(a = 1; a < argc; a++){
+    for(int a = 1; a < argc; a++){
         fp = fopen(argv[a], "r");
         if(fp != NULL){
-            while( (getline(&line, &len, fp)) != -1){
-                tok = strtok(line, " ");
-                while(tok){
-                    word_count = 0;
-                    for(i = 0; i < strlen(tok); i++){
-                        while(isdigit(tok[i])){
-                            nums[word_count++] = tok[i++];
-                        }
-                        loops = atoi(nums);
-                        printf("%d", loops);
-                        for(int d = 0; d < loops; d++){
-                            printf("%c", tok[i]);
-                        }
-                        memset(nums, 0, sizeof nums);
-                    }
-                tok = strtok(NULL, " ");
-                
-                printf(" ");
+            do{
+                count = fgetc(fp);
+                character = fgetc(fp);
+                if( feof(fp)){
+                    break;
                 }
-            printf("\n");
-            }
+                for(int b = 0; b <= count; b++){
+                    fputc(character, stdout);
+                }
+
+            }while(1);
         }
         else{
             printf("Could not read %s for file decompression!", argv[a]);
--- CSC331/Assignment 1/my-zip.c
+++ CSC331/Assignment 1/my-zip.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdint.h>
+#include <unistd.h>
 int main(int argc, char *argv[]){
 
 	FILE *fp;
-	int i;
-	int word_count;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	int a;
+	uint8_t character, new_character;
+	uint8_t count = 1;
 	if(argc < 2){
 		printf("my-zip: file1 [file2 ...] \n");
 		return(1);
 	}
-	for(a = 1; a < argc; a++){
+	for(int a = 1; a < argc; a++){
 		fp = fopen(argv[a], "r");
 		if(fp != NULL){
-			while( (nread = getline(&line, &len, fp)) != -1){
-				i = 0;
-				while(i < strlen(line)){
-					if(i > 0){
-						if(line[i-1] == line[i]){
-							word_count++;
-						}
-						else{
-							printf("%d%c", ++word_count, line[i - 1]);
-							word_count = 0;
-						}
-					}
-					i++;
-					printf("\n");
+			character = fgetc(fp);
+			count = 0;
+			do{
+				new_character = fgetc(fp);
+				if( feof(fp) ){
+					break;
 				}
-			}
-			fclose(fp);
-			free(line);
+				if(character == new_character){
+					count++;
+				}
+				else{
+					fputc(count, stdout);
+					fputc(character, stdout);
+					character = new_character;
+					count = 0;
+				}
+			}while(1);
 		}
 		else{
 			printf("Could not open %s for compressing!", argv[a]);
 			return(1);
 		}
+		fclose(fp);
 	}
 	return(0);
 }
