f9c24a19102e9ef76719ccab002d1996620ea547
Alex Vazquez <alex.vazquez708@gmail.com>
2019-04-25 10:33:20 -0400

    Testing mmap

---
+++ CSC331/Assignment 4/mapping.c
@@ -0,0 +1,28 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+size_t getFilesize(const char* filename){
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
+
+int main(int argc, char** argv){
+    size_t = getFilesize(argv[1]);
+    int fd = open(argv[1], O_RDONLY, 0);
+    assert (fd != -1);
+
+    void* mmappedData = mmap(NULL, filesize, PRO_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    assert(mmappedData !- MAP_FAILED);
+
+    write(1, mmappedData, filesize);
+
+    int rc = munmap(mmappedData, filesize);
+    assert(rc == 0);
+    close(fd);
+
+}
