19d9bc786bfbed989c29524aa2bace7c46d27d96
Alex Vazquez <alex.vazquez708@gmail.com>
2019-04-25 11:23:28 -0400

    More testing

--- CSC331/Assignment 4/mapping.c
+++ CSC331/Assignment 4/mapping.c
@@ -16,11 +16,10 @@ int main(int argc, char** argv){
     int fd = open(argv[1], O_RDONLY, 0);
     assert (fd != -1);
 
-    void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 5);
     assert(mmappedData != MAP_FAILED);
 
-    write(1, mmappedData, 5);
-    write(1, mmappedData, 5);
+    write(1, mmappedData, filesize);
 
     int rc = munmap(mmappedData, filesize);
     assert(rc == 0);
