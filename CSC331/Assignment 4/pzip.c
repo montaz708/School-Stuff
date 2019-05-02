d92e456bfb727bac60c1f2ea94cc8d3fe72aa2dd
Alex Vazquez <alex.vazquez708@gmail.com>
2019-05-02 11:22:17 -0400

    Test

--- CSC331/Assignment 4/pzip.c
+++ CSC331/Assignment 4/pzip.c
@@ -1,86 +1,95 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <pthread.h>
-<<<<<<< HEAD
-#include <sys/mman.h>
 #include <sys/types.h>
-#include <fcntrl.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
-#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <inttypes.h>
 
-void rle();
-size_t getFilesize(const char* filename);
-void *map(char* argv, int argc);
-
-int main(int argc, char *argv[]){
-
-	pthread_t *threads;
-	int i;
-	int file_count = 0;
-	int file_offset;
-	int num_threads = atoi(argv[1]);
-	if(argc < 2){
-		printf("pzip: num_threads file1 [file2 ...] \n");
-		return(1);
-	}
-	map();
-	threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
-	for(i = 0; i < num_threads; i++){
-		if(pthread_create(&threads[i], NULL, rle, ) != 0){
-			perror("Can't create thread \n");
-			free(threads);
-			exit(-1);
-		}
-	}
-	free(threads);
-	return(0);
-}
-
+char* contents;
+int mod_result;
+int div_result;
+static int offset = 0;
 
 size_t getFilesize(const char* filename){
-	struct stat st;
-	stat(filename, &st);
-	return st.st_size;
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
 }
 
-void *map(char *argv, int argc){
-	int file_offset;
-	size_t filesize;
-	void *mappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, FD, 0);
-	for(file_offset = 2; file_offset <= argc; file_offset++){
-		filesize = getFilesize(argv[file_offset]);
-		int fd = open(argv[file_offset], O_RDONLY, 0);
-		assert(fd != -1);
-	}
-}
-
-void rle(){
-	int fd = open(file, "r");
-		if(fp != NULL){
-			character = fgetc(fp); 
-			count = 0;
-			do{
-				new_character = fgetc(fp);
-				if( feof(fp) ){ 
-					break;
-				}
-				if(character == new_character){
-					count++;
-				}
-				else{
-					fwrite(&count, 4, 1, stdout);
-					fwrite(&character, 1, 1, stdout);
-					character = new_character;
-					count = 0;
-				}
-			}while(1);
+void rle(char *buff){
+	uint8_t character, new_character;
+	uint8_t count = 1;
+	int pos = 0;
+	character = buff[pos];
+	do{
+		pos++;
+		new_character = buff[pos];
+		printf("%c\n", buff[pos]);
+		if(new_character == '\0'){
+			break;
+		}
+		if(character == new_character){
+			count++;
 		}
 		else{
-			printf("Could not open %s for compressing!", argv[a]);
-			return(1);
+			fwrite(&count, 4, 1, stdout);
+			fwrite(&character, 1, 1, stdout);
+			character = new_character;
+			count = 1;
 		}
-		fclose(fp);
->>>>>>> 36b198a2cd7268c0f6c6cafd93cd6f20450cc43c
+	}while(1);
+	return;
+}
+
+void *split(){
+	char subbuff[div_result + mod_result + 1];
+	memcpy(subbuff, &contents[offset], div_result);
+	subbuff[div_result + mod_result] = '\0';
+	rle(subbuff);
+	offset = offset + div_result;
+}
+
+int main(int argc, char** argv){
+    int total_size = 0;
+    int num_threads = atoi(argv[1]);
+    char * buffer = (char *)malloc(2000*sizeof(char));
+    FILE *fp;
+    int a = 3;
+    total_size = total_size + getFilesize(argv[2]);
+    contents = (char *)malloc(total_size*sizeof(char));
+    fp = fopen(argv[2], "r");
+    if(!fp) perror("Nope \n"), exit(1);
+    if(1 != fread(contents, getFilesize(argv[2]), 1, fp))
+        fclose(fp), free(contents), fputs("Failed to read file \n", stderr), exit(1);
+    fclose(fp);
+    //argc-1 because the last argument in argv is (null)
+    while(a <= argc-1){
+        total_size = total_size + getFilesize(argv[a]);
+        contents = (char *)realloc(contents, total_size);
+        fp = fopen(argv[a], "r");
+        if(1 != fread(buffer, getFilesize(argv[a]), 1, fp))
+            fclose(fp), free(buffer), fputs("Failed to read file \n", stderr), exit(1);
+        strcat(contents, buffer);
+        memset(buffer, '\0', 2000*sizeof(char));
+        fclose(fp);
+        a++;
+    }
+
+	mod_result = total_size % num_threads;
+	div_result = total_size / num_threads;
+	pthread_t *threads = (pthread_t *)malloc((num_threads + 1)*sizeof(pthread_t));
+	for(int a = 0; a < num_threads + 1; a++){
+		pthread_create(&threads[a], NULL, split, NULL);
+		a++;
+	}
+	for(int b = 0; b < num_threads; b++){
+		pthread_join(threads[b], NULL);
+	}
+
+    free(contents);
+    free(buffer);
+    return(0);
 }
