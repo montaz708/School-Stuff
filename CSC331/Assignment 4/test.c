86f4395612605de9f8deee7d416dc3df12e055e2
Alex Vazquez <alex.vazquez708@gmail.com>
2019-05-02 08:34:34 -0400

    Test

--- CSC331/Assignment 4/test.c
+++ CSC331/Assignment 4/test.c
@@ -61,11 +61,9 @@ int main(int argc, char* argv[]){
 
 	mod_result = total_size % num_threads;
 	div_result = total_size / num_threads;
-	printf("total: %d \t mod: %d \t div: %d \t num: %d \n", total_size, mod_result, div_result, num_threads);
 	pthread_t *threads = (pthread_t *)malloc((num_threads + 1)*sizeof(pthread_t));
 	for(int a = 0; a < num_threads + 1; a++){
 		pthread_create(&threads[a], NULL, split, NULL);
-		printf("Loop: %d \n", a);
 		a++;
 	}
 	for(int b = 0; b < num_threads; b++){
