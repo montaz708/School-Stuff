e5e015f883c9f4d2cf682b757ffe6e7b37829f70
montaz708 <alex.vazquez708@gmail.com>
2019-05-02 07:31:48 -0400

    Good

--- CSC331/Assignment 4/mapping.c
+++ CSC331/Assignment 4/mapping.c
@@ -1,11 +1,13 @@
-#include <sys/mman.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+char* contents;
 
 size_t getFilesize(const char* filename){
     struct stat st;
@@ -13,19 +15,38 @@ size_t getFilesize(const char* filename){
     return st.st_size;
 }
 
+void *split(char subbuff){
+    printf("%s \n", subbuff);
+    return;
+}
+
 int main(int argc, char** argv){
-    size_t filesize = getFilesize(argv[1]);
-    int num = atoi(argv[2]);
-    int fd = open(argv[1], O_RDONLY, 0);
-    assert (fd != -1);
+    int total_size = 0;
+    int num_threads = atoi(argv[1]);
+    char * buffer = (char *)malloc(2000*sizeof(char));
+    FILE *fp;
+    int a = 3;
+    total_size = total_size + getFilesize(argv[2]);
+    contents = (char *)malloc(total_size*sizeof(char));
+    fp = fopen(argv[2], "r");
+    if(!fp) perror("Nope \n"), exit(1);
+    if(1 != fread(contents, getFilesize(argv[2]), 1, fp))
+        fclose(fp), free(contents), fputs("Failed to read file \n", stderr), exit(1);
+    fclose(fp);
+    //argc-1 because the last argument in argv is (null)
+    while(a <= argc-1){
+        total_size = total_size + getFilesize(argv[a]);
+        contents = (char *)realloc(contents, total_size);
+        fp = fopen(argv[a], "r");
+        if(1 != fread(buffer, getFilesize(argv[a]), 1, fp))
+            fclose(fp), free(buffer), fputs("Failed to read file \n", stderr), exit(1);
+        strcat(contents, buffer);
+        memset(buffer, '\0', 2000*sizeof(char));
+        fclose(fp);
+        a++;
+    }
 
-    unsigned char* mmappedData = (char *) mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
-    assert(mmappedData != MAP_FAILED);
-
-    printf("%s \n", mmappedData[0]);
-
-    int rc = munmap(mmappedData, filesize);
-    assert(rc == 0);
-    close(fd);
-
+    free(contents);
+    free(buffer);
+    return(0);
 }
